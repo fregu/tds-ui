a70f6235fed93366debe9a587e2ecac1d98feda5
Fredrik Söderquist <fregu808@gmail.com>
2019-06-17 23:38:36 +0200

    tabs with basic styling and minHeight

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,8 +1,8 @@
 // @flow
-import React, { type Node } from 'react'
+import React, { type Node,Component } from 'react'
 import classNames from 'classnames/bind'
 import { Redirect } from 'react-router'
-import Button from 'ui/conponents/Button'
+import Button from 'ui/components/Button'
 import State from 'ui/helpers/State'
 import styles from './index.css'
 const cx = classNames.bind(styles)
@@ -22,48 +22,67 @@ export type Props = {
   items: Array<TabProps>
 }
 
-export default function Tabs({
-  className,
-  items,
-  selectedTabIndex = 0
-}: Props) {
-  return (
-    <State state={{ selectedTabIndex }}>
-      {({ selectedTabIndex, setState }) => {
-        const selectedTab = items[selectedTabIndex]
-        return (
-          <div className={cx('Tabs', className)}>
-            <ul className={cx('Tabs-list')} role="tablist">
-              {items.map(({ title, disabled, className }, index) => (
-                <li className={cx('Tabs-item')}>
-                  <Button
-                    className={cx('Tabs-tab', className, {
-                      'Tabs-tab--selected': index === selectedTabIndex
-                    })}
-                    plain
-                    disabled={disabled}
-                    role="tab"
-                    text={title}
-                    aria-selected={
-                      index === selectedTabIndex ? 'true' : 'false'
-                    }
-                    onClick={() => setState({ selectedTabIndex: index })}
-                  />
-                </li>
-              ))}
-            </ul>
-            <div className={cx('Tabs-content')}>
-              {selectedTab.to ? (
-                <Redirect to={selectedTab.to} />
-              ) : (
-                <section className={cx('Tabs-panel')} role="tabpanel">
-                  {selectedTab.content}
-                </section>
-              )}
-            </div>
-          </div>
-        )
-      }}
-    </State>
-  )
+type StateType = {
+  selectedTabIndex?: number,
+  minHeight?: number
+}
+
+export default class Tabs extends Component<Props, StateType> {
+  state = {}
+  el: HTMLElement
+  onClick = (index) => {
+    if (this.el && typeof window !== 'undefined') {
+      const windowHeight = window.innerHeight
+      const bounds = this.el.getBoundingClientRect()
+      const minHeight = bounds.height
+
+      this.setState({
+        selectedTabIndex: index,
+        minHeight
+      })
+    }
+  }
+  render() {
+    const {
+      className,
+      items,
+      selectedTabIndex: selectedTabIndexProp = 0
+    } = this.props
+
+    const { selectedTabIndex = selectedTabIndexProp, minHeight } = this.state
+    const selectedTab = items[selectedTabIndex]
+
+    return (
+      <div ref={el => el && (this.el = el)} className={cx('Tabs', className)} style={{ minHeight: minHeight ? +minHeight + 'px' : 0 }}>
+        <ul className={cx('Tabs-list')} role="tablist">
+          {items.map(({ title, disabled, className }, index) => (
+            <li className={cx('Tabs-item')}>
+              <Button
+                className={cx('Tabs-tab', className, {
+                  'Tabs-tab--selected': index === selectedTabIndex
+                })}
+                plain
+                disabled={disabled}
+                role="tab"
+                text={title}
+                aria-selected={
+                  index === selectedTabIndex ? 'true' : 'false'
+                }
+                onClick={() => this.onClick(index)}
+              />
+            </li>
+          ))}
+        </ul>
+        <div className={cx('Tabs-content')}>
+          {selectedTab.to ? (
+            <Redirect to={selectedTab.to} />
+          ) : (
+            <section className={cx('Tabs-panel')} role="tabpanel">
+              {selectedTab.content}
+            </section>
+          )}
+        </div>
+      </div>
+    )
+  }
 }
