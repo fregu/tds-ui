019dcf00df2b40501509a2e098e7deddd463f358
Christine Tintin  Boghammar <christine@boghammar.se>
2019-06-19 17:04:33 +0200

    styling updates..

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 import * as iconset from 'ui/assets/icons'
 import classNames from 'classnames/bind'
 import styles from './index.css'
-const cs = classNames.bind(styles)
+const cx = classNames.bind(styles)
 
 export type Props = {
   type?: string,
@@ -14,12 +14,13 @@ export type Props = {
 }
 
 export default function Icon({ type, size, className, theme }: Props) {
-
   return iconset[type] ? (
-    <span className={cx('Icon', classNames, {
-      [`Icon--themed theme-${theme || ''}`]: theme,
-      ['Icon--' + (size || '')]: size
-    })>
+    <span
+      className={cx('Icon', className, {
+        [`Icon--themed theme-${theme || ''}`]: theme,
+        ['Icon--' + (size || '')]: size
+      })}
+    >
       <span
         className="Icon-wrapper"
         dangerouslySetInnerHTML={{ __html: iconset[type] }}
