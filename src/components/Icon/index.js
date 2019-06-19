225f1cd89707923e206cac294b832bc5b8a662b7
Christine Tintin  Boghammar <christine@boghammar.se>
2019-06-19 16:48:44 +0200

    default to fetch assets from ui

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react'
-import * as iconset from 'assets/icons'
+import * as iconset from 'ui/assets/icons'
 import classNames from 'classnames/bind'
 import styles from './index.css'
 const cs = classNames.bind(styles)
