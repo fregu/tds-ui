b63b2a3254dfde194692a28ab1643e1a2ebd1d80
Fredrik Söderquist <fredrik.soderquist@jayway.com>
2019-08-08 10:44:40 +0200

    Carousel scroll touch and snap

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -14,13 +14,17 @@ export type ItemProps = {
 export type Props = {
   items: Array<ItemProps>,
   className?: string,
-  activeIndex?: number
+  activeIndex?: number,
+  controls?: boolean
 }
 type State = {
   activeIndex: number
 }
+let touching
 let scrollingTimeout
 let isAnimating
+let isReversedDirection
+
 export default class Carousel extends Component<Props, State> {
   el: HTMLElement
   wrapper: HTMLElement
@@ -54,34 +58,102 @@ export default class Carousel extends Component<Props, State> {
   }
   onScroll = () => {
     const { activeIndex } = this.state
-    if (!isAnimating) {
-      clearTimeout(scrollingTimeout)
-      scrollingTimeout = setTimeout(() => this.scrollStop(true), 250)
-      const wrapperWidth = this.wrapper.clientWidth
-      const currentIndex = Math.round(this.wrapper.scrollLeft / wrapperWidth)
-      if (activeIndex !== currentIndex) {
-        this.setState({ activeIndex: currentIndex })
-      }
+    clearTimeout(scrollingTimeout)
+    if (!isAnimating && !touching) {
+      scrollingTimeout = setTimeout(() => this.scrollStop(true), 50)
+    }
+    const wrapperWidth = this.wrapper.clientWidth
+    const childEls = this.items.children
+    const scrollLeft = this.wrapper.scrollLeft
+
+    // calculate which item is closest to current scrolling position
+    const currentIndex = Array.prototype.reduce.call(
+      childEls,
+      (closestIndex, el, index) => {
+        const scrollPos = isReversedDirection
+          ? scrollLeft + wrapperWidth
+          : scrollLeft
+        const elPos = isReversedDirection
+          ? el.offsetLeft + el.clientWidth
+          : el.offsetLeft
+
+        const closestPos = isReversedDirection
+          ? childEls[closestIndex].offsetLeft +
+            childEls[closestIndex].clientWidth
+          : childEls[closestIndex].offsetLeft
+
+        return Math.abs(scrollPos - elPos) < Math.abs(scrollPos - closestPos)
+          ? index
+          : closestIndex
+      },
+      0
+    )
+
+    if (activeIndex !== currentIndex) {
+      this.setState({ activeIndex: currentIndex })
     }
   }
   scrollStop = (snapToActive?: boolean) => {
     clearTimeout(scrollingTimeout)
 
     const { activeIndex } = this.state
-    if (isAnimating) {
+
+    // Do not snap to closes if carousel is scrolled to the end of start
+    if (
+      this.wrapper.scrollLeft >=
+      this.items.clientWidth - this.wrapper.clientWidth
+    ) {
+      isReversedDirection = true
+      this.setState({ activeIndex: this.items.children.length - 1 })
+    } else if (this.wrapper.scrollLeft <= 0) {
+      isReversedDirection = false
+      this.setState({ activeIndex: 0 })
+    } else if (snapToActive) {
+      this.scrollToItem(activeIndex, 250).then(() => {
+        isAnimating = false
+      })
+    } else if (isAnimating) {
       isAnimating = false
     }
-    if (snapToActive) {
-      this.scrollToItem(activeIndex, 250)
-    }
+  }
+  scrollTo = (leftPos, duration = 0) => {
+    return new Promise(resolve =>
+      scrollTo(this.wrapper, leftPos, duration, 'scrollLeft', resolve)
+    )
   }
   scrollToItem = (index: number, duration: number = 500) => {
     const wrapperWidth = this.wrapper.clientWidth
     clearTimeout(scrollingTimeout)
-    isAnimating = true
-    scrollTo(this.wrapper, wrapperWidth * index, duration, 'scrollLeft', () =>
-      this.scrollStop()
-    )
+    isAnimating = duration > 0
+
+    const leftPos = isReversedDirection
+      ? this.items.children[index].offsetLeft +
+        this.items.children[index].clientWidth -
+        this.wrapper.clientWidth
+      : this.items.children[index].offsetLeft
+
+    return this.scrollTo(leftPos, duration)
+  }
+  onMouseDown = e => {
+    touching = {
+      start: e.pageX,
+      scroll: this.wrapper.scrollLeft
+    }
+    document.addEventListener('mousemove', this.onMouseMove)
+    document.addEventListener('mouseleave', this.onMouseUp)
+    document.addEventListener('mouseup', this.onMouseUp)
+  }
+  onMouseMove = e => {
+    if (touching && e.pageX) {
+      this.scrollTo(touching.scroll + touching.start - e.pageX)
+    }
+  }
+  onMouseUp = e => {
+    touching = false
+    this.scrollStop(true)
+    document.removeEventListener('mousemove', this.onMouseMove)
+    document.removeEventListener('mouseleave', this.onMouseUp)
+    document.removeEventListener('mouseup', this.onMouseUp)
   }
   componentDidMount = () => {
     if (this.props.activeIndex) {
@@ -89,60 +161,75 @@ export default class Carousel extends Component<Props, State> {
     }
 
     this.wrapper.addEventListener('scroll', this.onScroll)
+    this.wrapper.addEventListener('mousedown', this.onMouseDown)
     if (typeof window !== 'undefined') {
       window.addEventListener('resize', this.onResize)
     }
   }
+  componentWillUnmount = () => {
+    this.wrapper.removeEventListener('scroll', this.onScroll)
+    this.wrapper.removeEventListener('mousedown', this.mouseDown)
+    document.removeEventListener('mousemove', this.onMouseMove)
+    document.removeEventListener('mouseleave', this.onMouseUp)
+    document.removeEventListener('mouseup', this.onMouseUp)
+  }
   render() {
-    const { items = [], className } = this.props
+    const { items = [], className, multiple, controls } = this.props
     const { activeIndex } = this.state
 
     return (
       <div
         ref={el => el && (this.el = el)}
-        className={cx('Carousel', className)}
+        className={cx('Carousel', className, {
+          'Carousel--multiple': multiple
+        })}
       >
-        <div className={cx('Carousel-actions')}>
-          {activeIndex > 0 ? (
-            <Button
-              className={cx('Carousel-stepLeft')}
-              icon={{ type: 'chevronLeft' }}
-              onClick={this.stepLeft}
-              text="left"
-            />
-          ) : null}
-          {activeIndex < items.length - 1 ? (
-            <Button
-              className={cx('Carousel-stepRight')}
-              icon={{ type: 'chevronRight' }}
-              onClick={this.stepRight}
-              text="right"
-            />
-          ) : null}
-        </div>
+        {controls ? (
+          <div className={cx('Carousel-actions')}>
+            {activeIndex > 0 ? (
+              <Button
+                className={cx('Carousel-stepLeft')}
+                icon={{ type: 'chevronLeft' }}
+                onClick={this.stepLeft}
+                text="left"
+              />
+            ) : null}
+            {activeIndex < items.length - 1 ? (
+              <Button
+                className={cx('Carousel-stepRight')}
+                icon={{ type: 'chevronRight' }}
+                onClick={this.stepRight}
+                text="right"
+              />
+            ) : null}
+          </div>
+        ) : null}
         <div
           ref={el => el && (this.wrapper = el)}
           className={cx('Carousel-wrapper')}
         >
-          <div
-            ref={el => el && (this.items = el)}
-            className={cx('Carousel-items')}
-          >
-            {items.map(({ image, content, isActive }, index) => (
-              <div
-                className={cx('Carousel-item', {
-                  'Carousel-item--image': image
-                })}
-              >
-                {image ? (
-                  <Figure
-                    {...image}
-                    className={cx('Carousel-image', image.className)}
-                  />
-                ) : null}
-                {content}
-              </div>
-            ))}
+          <div className={cx('Carousel-innerWrapper')}>
+            <div
+              ref={el => el && (this.items = el)}
+              className={cx('Carousel-items')}
+            >
+              {items.map(({ id, image, content, isActive }, index) => (
+                <div
+                  key={`carouselitem-${id || index}`}
+                  className={cx('Carousel-item', {
+                    'Carousel-item--image': image
+                  })}
+                >
+                  {image ? (
+                    <Figure
+                      {...image}
+                      className={cx('Carousel-image', image.className)}
+                    />
+                  ) : null}
+                  {content}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
